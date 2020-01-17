d831600cfe37605c976976edbe9e2b1ddd83b4d1
reaganiwadha <reaganiwadha@protonmail.com>
2020-01-17 15:09:04 +0700

    Initialize Styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Starships from './entities/Starships'
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header stars">
+        <h1 className="Title">Title</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +29,22 @@ function App() {
         >
           Learn React
         </a>
+
+        <Link to="/starships"> Click me to show the starships component</Link>
+
       </header>
+
+      <Switch>
+        <Route path="/starships">
+          <Starships />
+        </Route>
+        <Route path="/persons">
+          <Persons />
+        </Route>
+        <Route path="/assets">
+          <Assets />
+        </Route>
+      </Switch>
     </div>
   );
 }
