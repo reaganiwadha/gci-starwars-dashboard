1026bf949643cb5ad000be4e3016c304e520f811
reaganiwadha <reaganiwadha@protonmail.com>
2020-01-18 11:15:07 +0700

    Updates

---
+++ precache-manifest.9dfc4f61f5e80adaac5aa4e95d56859e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8d82388846cf26bb90a38c3cdffb37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aec66dc510345acf0e7",
+    "url": "/static/css/2.79966c17.chunk.css"
+  },
+  {
+    "revision": "86c8b99a0a8e50b17790",
+    "url": "/static/css/main.9b06ec5f.chunk.css"
+  },
+  {
+    "revision": "3aec66dc510345acf0e7",
+    "url": "/static/js/2.b74f7e87.chunk.js"
+  },
+  {
+    "revision": "86c8b99a0a8e50b17790",
+    "url": "/static/js/main.ef8dcd1b.chunk.js"
+  },
+  {
+    "revision": "358f4e27d6d35578440d",
+    "url": "/static/js/runtime-main.f764344c.js"
+  }
+]);
